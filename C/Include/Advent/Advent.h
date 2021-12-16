89db808e18d974ff82288d826b17fdb9263412ac
Kyle Coffey <mariorules24@gmail.com>
2021-12-16 14:22:25 -0600

    embed input in executable

--- C/Include/Advent/Advent.h
+++ C/Include/Advent/Advent.h
@@ -1,12 +1,6 @@
 #pragma once
 
 #include "AdventConfig.h"
-#if __has_include("DayConfig.h")
-#include "DayConfig.h"
-#endif
-
-#define ADVENT_INPUT_FILE_NAME ADVENT_DAY_NAME "Input.txt"
+#include <stddef.h>
 
 #define NS_(x) Advent_##x
-
-char* NS_(ReadPuzzleInput)(const char* inputFileName);
--- C/Source/Day01/Main.c
+++ C/Source/Day01/Main.c
@@ -1,9 +1,8 @@
 #include "Advent/Advent.h"
+#include "Input.h"
 #include <assert.h>
 
 int main(void)
 {
-  char* puzzleInput = Advent_ReadPuzzleInput(ADVENT_INPUT_FILE_NAME);
-  assert(puzzleInput);
   return 0;
 }
