9a33e1ac58f4a52e7946db40b101e9d1a6992530
Kyle Coffey <mariorules24@gmail.com>
2021-12-17 13:17:03 -0600

    broken day03

--- C/Source/Day03/Main.c
+++ C/Source/Day03/Main.c
@@ -1,11 +1,17 @@
 #include <Advent/Advent.h>
 #include <DayConfig.h>
 #include <Input.h>
+#include <string.h>
 #include <unicode/ustdio.h>
 #include <unicode/ustring.h>
 
 static void PrintHelp(void);
 static void PrintVersion(void);
+
+typedef struct
+{
+  size_t c[2];
+} Count;
 
 int main(int argc, char** argv)
 {
@@ -39,6 +45,54 @@ int main(int argc, char** argv)
     fprintf(stderr, "Unicode error: %s\n", u_errorName(errorCode));
     return 3;
   }
+
+  Count* counts = NULL;
+  size_t width = 0;
+
+  U_STRING_DECL(delim, "\r\n", 2);
+  UChar* saveptr;
+  for (UChar* iter = u_strtok_r(unicodeInput, delim, &saveptr); iter != NULL; iter = u_strtok_r(NULL, delim, &saveptr))
+  {
+    size_t i = 0;
+    while (true)
+    {
+      UChar32 ci;
+      U16_NEXT(iter, i, -1, ci);
+      if (ci == 0)
+      {
+        break;
+      }
+      if (ci == '0' || ci == '1')
+      {
+        if (width < i || counts == NULL)
+        {
+          Count* newCounts = realloc(counts, sizeof(Count) * (i + 1));
+          if (newCounts == NULL)
+          {
+            fprintf(stderr, "Memory allocation failure\n");
+            return 4;
+          }
+          counts = newCounts;
+          memset(&counts[i], 0, sizeof(Count));
+          width = i;
+        }
+        counts[i].c[ci - '0']++;
+      }
+    }
+  }
+
+  uint64_t gamma = 0;
+  uint64_t epsilon = 0;
+  for (size_t i = 0; i < width; ++i)
+  {
+    gamma <<= 1;
+    gamma |= counts[i].c[1] > counts[i].c[0];
+    printf("%lx\n", gamma);
+    epsilon <<= 1;
+    epsilon |= !(gamma & 1);
+  }
+
+  printf("%lu\n", gamma * epsilon);
 
   free(unicodeInput);
 }
